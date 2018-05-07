2d8dee051ae6e8eebe6d390e942fc17d1e28b83b
kchuriumova <kseniia.churiumova@venmo.com>
2018-05-07 11:01:15 -0400

    update 0.0.2

--- nose_timeout/__init__.py
+++ nose_timeout/__init__.py
@@ -1,5 +1,5 @@
 """Distribute your nose tests across multiple machines, hassle-free"""
-VERSION = (0, 0, 1, '')
+VERSION = (0, 0, 2, '')
 __version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
 __author__ = 'Kseniia Churiumova'
 __contact__ = 'andreychukks@gmail.com'
