1d9413e0fcf8c8baec3036a514ef5136103d5839
Israel Fruchter <fruch@scylladb.com>
2019-05-13 20:43:56 +0300

    Add support for backtraces of threads

--- nose_timeout/plugin.py
+++ nose_timeout/plugin.py
@@ -1,9 +1,11 @@
+import sys
 import logging
 import signal
-
+import threading
+import traceback
 
 from nose.plugins.base import Plugin
-
+from backports.shutil_get_terminal_size import get_terminal_size
 
 logger = logging.getLogger('nose.plugins.nose_timeout')
 
@@ -59,6 +61,7 @@ class NoseTimeout(Plugin):
         if test_timeout:
             def timeout(result):
                 def __handler(signum, frame):
+                    dump_stacks()
                     msg = "Function execution is longer than %s second(s). Aborted." % test_timeout
                     raise TimeoutException(msg)
 
@@ -68,7 +71,50 @@ class NoseTimeout(Plugin):
                     test.test(result)
                 except TimeoutException as e:
                     print(e.message + " timeout")
+
                 finally:
                     signal.signal(signal.SIGALRM, sig_handler)
                     signal.alarm(0)
             return timeout
+
+
+def dump_stacks():
+    """Dump the stacks of all threads except the current thread"""
+    current_ident = threading.current_thread().ident
+    for thread_ident, frame in sys._current_frames().items():
+        if thread_ident == current_ident:
+            continue
+        for t in threading.enumerate():
+            if t.ident == thread_ident:
+                thread_name = t.name
+                break
+        else:
+            thread_name = '<unknown>'
+        write_title('Stack of %s (%s)' % (thread_name, thread_ident))
+        write(''.join(traceback.format_stack(frame)))
+
+
+def write_title(title, stream=None, sep='~'):
+    """Write a section title
+
+    If *stream* is None sys.stderr will be used, *sep* is used to
+    draw the line.
+    """
+    if stream is None:
+        stream = sys.stdout
+    width, _ = get_terminal_size()
+    fill = int((width - len(title) - 2) / 2)
+    line = ' '.join([sep * fill, title, sep * fill])
+    if len(line) < width:
+        line += sep * (width - len(line))
+    stream.write('\n' + line + '\n')
+
+
+def write(text, stream=None):
+    """Write text to stream
+
+    Pretty stupid really, only here for symetry with .write_title().
+    """
+    if stream is None:
+        stream = sys.stdout
+    stream.write(text)
